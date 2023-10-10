771ce1790d68cc80542f2845265c9f15be74eee7
gra-m <grahamd483@gmail.com>
2023-10-10 15:57:22 +0200

    168b. DesktopLauncher window configuration and GameRenderer fields initialised.

--- core/src/fun/madeby/snake/game/GameRenderer.java
+++ core/src/fun/madeby/snake/game/GameRenderer.java
@@ -1,4 +1,41 @@
 package fun.madeby.snake.game;
 
-public class GameRenderer {
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.utils.Disposable;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+import fun.madeby.snake.config.GameConfig;
+import fun.madeby.snake.util.debug.DebugCameraController;
+
+// super dispose in game disposes game screen and this dispose
+public class GameRenderer implements Disposable {
+    private final GameController controller;
+
+    private OrthographicCamera camera;
+    private Viewport viewport;
+    private ShapeRenderer renderer;
+    private DebugCameraController debugCameraController;
+
+    public GameRenderer(GameController gameController) {
+        this.controller = gameController;
+        init();
+    }
+
+    private void init() {
+        camera = new OrthographicCamera();
+        viewport = new FitViewport(GameConfig.WORLD_WIDTH, GameConfig.WORLD_HEIGHT, camera);
+        renderer = new ShapeRenderer();
+
+        debugCameraController = new DebugCameraController();
+        debugCameraController.setStartPosition(GameConfig.WORLD_CENTER_X, GameConfig.WORLD_CENTER_Y);
+    }
+
+
+    @Override
+    public void dispose() {
+    renderer.dispose();
+
+    }
 }
