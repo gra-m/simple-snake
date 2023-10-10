b18a53e91305e8830e917412873e37e3a2f669ea
gra-m <grahamd483@gmail.com>
2023-10-10 15:42:43 +0200

    168a. make batch and assetmanager available from game, dispose of them

--- core/src/fun/madeby/SimpleSnakeGame.java
+++ core/src/fun/madeby/SimpleSnakeGame.java
@@ -1,15 +1,41 @@
 package fun.madeby;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Logger;
 
 import fun.madeby.snake.game.GameScreen;
 
-// As opposed to ApplicationAdapter Game can multiple screens
+// As opposed to ApplicationAdapter Game can have multiple screens
 public class SimpleSnakeGame extends Game {
+    private AssetManager assetManager;
+    private SpriteBatch batch;
     @Override
     public void create() {
         //todo if debugger not working
         //Gdx.app.setLogLevel(Application.LOG_DEBUG);
+
+        assetManager = new AssetManager();
+        assetManager.getLogger().setLevel(Logger.DEBUG);
+
+        batch = new SpriteBatch();
+
         setScreen(new GameScreen(this));
+    }
+
+    @Override
+    public void dispose() {
+       super.dispose();
+       assetManager.dispose();
+       batch.dispose();
+    }
+
+    public AssetManager getAssetManager() {
+        return assetManager;
+    }
+
+    public SpriteBatch getBatch() {
+        return batch;
     }
 }
