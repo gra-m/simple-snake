adb7642be6068c17b2d0a2cdc6124c72782cf14e
gra-m <grahamd483@gmail.com>
2023-10-11 18:48:36 +0200

    179. got all of the assets together, there is no asset packer in libGDX 3, so completed in legacy project.

---
+++ core/src/fun/madeby/snake/assets/AssetPaths.java
@@ -0,0 +1,8 @@
+package fun.madeby.snake.assets;
+
+public class AssetPaths {
+    public static final String UI_FONT_OSWALD_32 = "ui/fonts/oswald-32.fnt";
+    public static final String GAMEPLAY_ATLAS = "gameplay/gameplay.atlas";
+    public static final String UI_SKIN = "ui/ui.json";
+    private AssetPaths(){}
+}
---
+++ core/src/fun/madeby/snake/assets/RegionNames.java
@@ -0,0 +1,11 @@
+package fun.madeby.snake.assets;
+
+// names as specified in atlas or skin
+public class RegionNames {
+    public static final String BACKGROUND = "background";
+    public static final String BODY = "body";
+    public static final String COIN = "coin";
+    public static final String HEAD = "head";
+
+    private RegionNames(){}
+}
