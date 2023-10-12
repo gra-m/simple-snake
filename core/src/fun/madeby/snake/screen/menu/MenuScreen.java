b5492a4bf05a12a70320c89f94dc527317511007
gra-m <grahamd483@gmail.com>
2023-10-12 16:45:22 +0200

    182b. createUi() creates table, fills with UI attributes, activates and packs them

--- core/src/fun/madeby/snake/screen/menu/MenuScreen.java
+++ core/src/fun/madeby/snake/screen/menu/MenuScreen.java
@@ -1,17 +1,27 @@
 package fun.madeby.snake.screen.menu;
 
+import static com.badlogic.gdx.scenes.scene2d.ui.Table.Debug.table;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
 import fun.madeby.SimpleSnakeGame;
 import fun.madeby.snake.assets.AssetDescriptors;
+import fun.madeby.snake.assets.ButtonStyleNames;
+import fun.madeby.snake.assets.RegionNames;
 import fun.madeby.snake.config.GameConfig;
 
 public class MenuScreen extends ScreenAdapter {
@@ -41,7 +51,42 @@ public class MenuScreen extends ScreenAdapter {
     }
 
     private Actor createUI() {
-        return null;
+
+        // create and activate elements
+        Table menuActorTable = new Table(skin);
+        TextureRegion background = assetManager.get(RegionNames.BACKGROUND);
+        TextureRegionDrawable tableBackground = new TextureRegionDrawable(background);
+        Image titleImage = new Image(skin, RegionNames.TITLE);
+        Button playButton = new Button(skin, ButtonStyleNames.PLAY);
+        Button quitButton = new Button(skin, ButtonStyleNames.QUIT);
+
+        playButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                play();
+            }
+        });
+
+        quitButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                quit();
+            }
+        });
+
+        // set table attributes
+        menuActorTable.defaults().pad(10);
+        menuActorTable.setBackground(tableBackground);
+        menuActorTable.center();
+        menuActorTable.setFillParent(true);
+
+        // pack table
+        menuActorTable.add(titleImage);
+        menuActorTable.add(playButton);
+        menuActorTable.add(quitButton);
+        menuActorTable.pack();
+
+        return menuActorTable;
     }
 
     @Override
